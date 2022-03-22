994b54ee0d9f657b186076c15027606363c6811c
Voynitskiy | AlxVoy <85261070+Voynitskiy@users.noreply.github.com>
2022-03-23 01:12:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var indices = [];
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+var element = 'a';
+var idx = array.indexOf(element);
+while (idx != -1) {
+  indices.push(idx);
+  idx = array.indexOf(element, idx + 1);
+}
+
+console.log(indices);
+// [0, 2, 4]
